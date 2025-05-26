e9b9a5bc2b2e05c647bafd96721b6e0b5e1b3bc1
Peter Breslin <peter.breslin@outlook.com>
2025-05-26 18:14:38 +0100

    Add dashboard module + route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', loadChildren: () => import('./module/dashboard/dashboard.module').then(o => o.DashboardModule)},
+    { path:'dashboard', loadChildren: () => import('./module/dashboard/dashboard.module').then(o => o.DashboardModule)}
+];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  constructor(  private router:Router){
+  }
 
+  onNavigateTo(value:string){
+    this.router.navigate([value]);
+  }
 }
