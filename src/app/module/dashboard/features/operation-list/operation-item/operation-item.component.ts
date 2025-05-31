1b3b9f477690ada87ae093f2144b00b3fbf11618
Peter Breslin <peter.breslin@outlook.com>
2025-05-31 20:41:15 +0100

    ng-template example

--- src/app/module/dashboard/features/operation-list/operation-item/operation-item.component.ts
+++ src/app/module/dashboard/features/operation-list/operation-item/operation-item.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, TemplateRef } from '@angular/core';
+
+import { IOperation } from '../../dashboard/dashboard/services/operation-data/operation-interface-model';
 
 @Component({
   selector: 'app-operation-item',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './operation-item.component.html',
   styleUrl: './operation-item.component.css'
 })
-export class OperationItemComponent {
+export class OperationItemComponent implements AfterViewInit {
+  @Input() operation?:IOperation;
+  @Input() headerTemplate:TemplateRef<any> | undefined;
+  @Input() contentTemplate:TemplateRef<IOperation> | undefined;
 
+  // @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+  // @ViewChild('defaultTemplate') defaultTemplate!: TemplateRef<any>;
+
+  ngAfterViewInit(): void {
+  //  this.container.createEmbeddedView(this.defaultTemplate);
+  }
 }
+
--- src/app/module/dashboard/features/operation-list/operation-list.component.ts
+++ src/app/module/dashboard/features/operation-list/operation-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, inject } from '@angular/core';
 
 import { IOperation } from '../dashboard/dashboard/services/operation-data/operation-interface-model';
 import {OperationDataService} from '../dashboard/dashboard/services/operation-data/operation-data.service';
@@ -12,25 +12,12 @@ import { ReplaySubject, Subject, takeUntil } from 'rxjs';
 })
 export class OperationListComponent implements  OnInit, OnDestroy{
 
-  objectList:IOperation[]=[];
-  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  dataSource:IOperation[]=[];
+  destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  operationDataService = inject(OperationDataService)
 
-  constructor(private operationDataService:OperationDataService){}
-  
   ngOnInit(): void {
-    
-    this.operationDataService.get()
-    .pipe(takeUntil(this.destroyed$))
-    .subscribe({ next: async (o) => {
-
-      this.objectList = o;
-    },
-    error: (err) => {
-      this.onError()
-    },
-    complete: () => {
-      this.onComplete();
-    }});
+    this.setDataSource();
   }
 
   ngOnDestroy(): void {
@@ -41,4 +28,18 @@ export class OperationListComponent implements  OnInit, OnDestroy{
   onComplete() : void {}
 
   onError() : void {}
+
+  private setDataSource() : void {
+    this.operationDataService.get()
+    .pipe(takeUntil(this.destroyed$))
+    .subscribe({ next: async (o) => {
+      this.dataSource = o;
+    },
+    error: (err) => {
+      this.onError()
+    },
+    complete: () => {
+      this.onComplete();
+    }});
+  }
 }
