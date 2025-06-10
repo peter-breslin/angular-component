a7fa98f9a30370b932d8541ea3b8b1b5e8afe78e
Peter Breslin <peter.breslin@outlook.com>
2025-06-10 21:00:03 +0100

    add horizontal scrolling

--- src/app/module/dashboard/features/dashboard/dashboard/services/operation-data/mock-operation-data.ts
+++ src/app/module/dashboard/features/dashboard/dashboard/services/operation-data/mock-operation-data.ts
@@ -26,4 +26,56 @@ export const MockOperationData:Array<IOperation> = [{
      "status" : 3,
      "price": 79.060,
      "coupon":0.250
+},{
+    "isin" : "GB00BL68HJ26",
+    "type" : "GILT",
+    "title" : "Treasury 0.125% 30/01/2026",
+    "maturityDate" : "30 January 2026",
+    "status" : 1,
+    "price": 97.870,
+    "coupon":0.125
+},
+{
+    "isin" : "GB00BMBL1G81",
+    "type" : "GILT",
+    "title" : "Treasury 0.125% 31/01/2028",
+     "maturityDate" : "31 January 2028",
+     "status" : 2,
+     "price": 90.830,
+     "coupon":0.125
+},
+{
+    "isin" : "GB00BMGR2809 ",
+    "type" : "GILT",
+    "title" : "Treasury 0.25% 31/07/2031",
+     "maturityDate" : "31 July 2031",
+     "status" : 3,
+     "price": 79.060,
+     "coupon":0.250
+},{
+    "isin" : "GB00BL68HJ26",
+    "type" : "GILT",
+    "title" : "Treasury 0.125% 30/01/2026",
+    "maturityDate" : "30 January 2026",
+    "status" : 1,
+    "price": 97.870,
+    "coupon":0.125
+},
+{
+    "isin" : "GB00BMBL1G81",
+    "type" : "GILT",
+    "title" : "Treasury 0.125% 31/01/2028",
+     "maturityDate" : "31 January 2028",
+     "status" : 2,
+     "price": 90.830,
+     "coupon":0.125
+},
+{
+    "isin" : "GB00BMGR2809 ",
+    "type" : "GILT",
+    "title" : "Treasury 0.25% 31/07/2031",
+     "maturityDate" : "31 July 2031",
+     "status" : 3,
+     "price": 79.060,
+     "coupon":0.250
 }];
