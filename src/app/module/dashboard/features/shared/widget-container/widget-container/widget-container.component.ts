e5ed2d9b8688b51e337e47689af28fa1a07e1c98
Peter Breslin <peter.breslin@outlook.com>
2025-06-14 16:39:36 +0100

    Create component widget-container

---
+++ src/app/module/dashboard/features/shared/widget-container/widget-container/widget-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-container',
+  imports: [],
+  templateUrl: './widget-container.component.html',
+  styleUrl: './widget-container.component.css'
+})
+export class WidgetContainerComponent {
+
+}
