270462d97f8b88eba36f45b5713455519bf8e043
Peter Breslin <peter.breslin@outlook.com>
2025-06-17 20:46:48 +0100

    Add css to component

--- src/app/module/dashboard/features/dashboard/dashboard/services/statistics-data/mock-statistic-data.ts
+++ src/app/module/dashboard/features/dashboard/dashboard/services/statistics-data/mock-statistic-data.ts
@@ -14,4 +14,19 @@ export const MockData:Array<IStatistic> = [{
     "isin" : "GB00BL68HJ26",
     "name" : "Average Market Price",
     "value": 54.6756,
+},
+{
+    "isin" : "GB00BL68HJ26",
+    "name" : "TYW Price",
+    "value": 454.898,
+},
+{
+    "isin" : "GB00BL68HJ26",
+    "name" : "ZZ Price",
+    "value": 454.787,
+},
+{
+    "isin" : "GB00BL68HJ26",
+    "name" : "A Price",
+    "value": 45.567,
 }];
